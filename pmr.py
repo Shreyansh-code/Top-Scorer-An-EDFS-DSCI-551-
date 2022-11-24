82a32481625f41abde56850fd18204d62a23f0c2
Shreyansh Khandelwal <52644779+Shreyansh-code@users.noreply.github.com>
2022-11-23 19:55:47 -0800

    updated pmr

--- pmr.py
+++ pmr.py
@@ -9,24 +9,33 @@ db = mysql.connector.connect(
 )
 cursor = db.cursor()
 
-def goals(name,part,goal):
+def goals(name,goal):
+    ans=[]
     cursor.execute('use dsci551')
-    cursor.execute('select player_name from table_{0}_{1} where goals>{2};'.format(name,part,goal))
-    print(cursor.fetchall())
+    for i in range(1,4):
+        cursor.execute('select player_name from table_{0}_{1} where goals>{2};'.format(name,i,goal))
+        ans.append(cursor.fetchall())
+    print(ans)
 
 
 
-def xG(name,part,xG1, xG2):
+def xG(name,xG1, xG2):
+    ans=[]
     cursor.execute('use dsci551')
-    cursor.execute('select player_name from table_{0}_{1} where xG>{2} and xG<{3};'.format(name,part,xG1, xG2))
-    print(cursor.fetchall())
+    for i in range(1,4):
+        cursor.execute('select player_name from table_{0}_{1} where xG>{2} and xG<{3};'.format(name,i,xG1, xG2))
+        ans.append(cursor.fetchall())
+    print(ans)
 
 
 
 
-def searchGoals(name,part,goal):
+def searchGoals(name,goal):
+    ans =[]
     cursor.execute('use dsci551')
-    cursor.execute('select player_name from table_{0}_{1} where goals={2};'.format(name,part,goal))
-    print(cursor.fetchall())
+    for i in range(1,4):
+        cursor.execute('select player_name, goals from table_{0}_{1} where goals={2};'.format(name,i,goal))
+        ans.append(cursor.fetchall())
+    print(ans)
 
-searchGoals('eeplss', 1, 13)
+xG('eplGoal',1,2)
